5054fa995a8069ca4b56a91140f378d0e2ecd525
Andy Gallagher <andy.gallagher@theguardian.com>
2016-12-09 12:29:02 +0000

    add_encoding methods and tests

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ DATABASE = {
     'user': "ubuntu",
     'password': "",
     'host': 'localhost',
-    'unix_socket': "/opt/local/var/run/mysql56/mysqld.sock",
+    #'unix_socket': "/opt/local/var/run/mysql56/mysqld.sock",
     'database': "circle_test",
     'use_pure': True
 }
